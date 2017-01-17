355d2b4d0a28a6e07c16874eda4121cf45c19e72
patobes <pablo@tobes.es>
2017-01-17 17:55:55 +0100

    Delete inlinekeyboard Image route changed

--- Commands/PllCommand.php
+++ Commands/PllCommand.php
@@ -56,31 +56,31 @@ class PllCommand extends UserCommand
                     break;
                 case 'Ua':
                     $data['caption'] = "Ua: R2 U' R' U' R U R U R U' R";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
                 case 'Ub':
                     $data['caption'] = "Ub: R' U R' U' R' U' R' U R U R2";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'H':
                     $data['caption'] = "H: M2 U M2 U2 M2 U M2";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'Z':
                     $data['caption'] = "Z: U R' U' R U' R U R U' R' U R U R2 U' R' U";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'T':
                     $data['caption'] = "T: R U R' U' R' F R2 U' R' U' R U R' F'";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'Y':
                     $data['caption'] = "Y: F R U' R' U' R U R' F' R U R' U' R' F R F'";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'A':
@@ -94,11 +94,11 @@ class PllCommand extends UserCommand
                     break;
                 case 'Aa':
                     $data['caption'] = "Aa: L' B L' F2 L B' L' F2 L2";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
                 case 'Ab':
                     $data['caption'] = "Ab: x R2 D2 R U R' D2 R U' R";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'J':
@@ -112,11 +112,11 @@ class PllCommand extends UserCommand
                     break;
                 case 'Ja':
                     $data['caption'] = "Ja: R U R' F' R U R' U' R' F R2 U' R' U'";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
                 case 'Jb':
                     $data['caption'] = "Jb: L' U R' z R2 U R' U' R2 U D";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'R':
@@ -130,21 +130,21 @@ class PllCommand extends UserCommand
                     break;
                 case 'Ra':
                     $data['caption'] = "Ra: R' U2 R U2 R' F R U R' U' R' F' R2 U'";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
                 case 'Rb':
                     $data['caption'] = "Rb: R U R' F' R U2 R' U2 R' F R U R U2 R' U'";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'E':
                     $data['caption'] = "E: x' R U' R' D R U R' D' R U R' D R U' R' D'";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'F':
                     $data['caption'] = "F: R' U' F' R U R' U' R' F R2 U' R' U' R U R' U R";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'G':
@@ -158,19 +158,19 @@ class PllCommand extends UserCommand
                     break;
                 case 'Ga':
                     $data['caption'] = "Ga: R2 u R' R R' U' R u' R2 y L' U L";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
                 case 'Gb':
                     $data['caption'] = "Gb: R' U' R y R2 u R' U R U' R u R2";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
                 case 'Gc':
                     $data['caption'] = "Gc: R2 u' R U' R U R' u R2 f R f'";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
                 case 'Gd':
                     $data['caption'] = "Gd: R U R' y' R2 u' R U' R' U R' u R2";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'N':
@@ -184,16 +184,16 @@ class PllCommand extends UserCommand
                     break;
                 case 'Na':
                     $data['caption'] = "Na: z R' U R' D R2 U' R U D' R' D R2 U' R D'";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
                 case 'Nb':
                     $data['caption'] = "Nb: R' U R U' R' F' U' F R U R' F R' F' R U' R";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 case 'V':
                     $data['caption'] = "V: R' U R' U' y R' F' R2 U' R' U R' F R F";
-                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/test.jpg'));
+                    return Request::sendPhoto($data, realpath(__DIR__ . '/images/'.$command.'.jpg'));
                     break;
 
                 default:
